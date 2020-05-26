0f473d15acfbec12c156d12850e2b60a5e67700b
Sandro Ciervo <sandro.ciervo@live.com>
2020-05-26 09:53:27 +0200

    Remove seeds

--- PedaloWebApp/Infrastructure/Data/EntityConfigurations/CustomerConfiguration.cs
+++ PedaloWebApp/Infrastructure/Data/EntityConfigurations/CustomerConfiguration.cs
@@ -13,67 +13,6 @@
             builder.HasKey(x => x.CustomerId);
             builder.Property(x => x.FirstName).IsRequired().HasMaxLength(80);
             builder.Property(x => x.LastName).IsRequired().HasMaxLength(80);
-
-            Seed(builder);
-        }
-
-        private static void Seed(EntityTypeBuilder<Customer> builder)
-        {
-            builder.HasData(new Customer
-            {
-                FirstName = "Wilhelm",
-                LastName = "Tell",
-                BirthdayDate = new DateTime(1970, 1, 1),
-            });
-
-            builder.HasData(new Customer
-            {
-                FirstName = "Arthur",
-                LastName = "Dent",
-                BirthdayDate = new DateTime(1970, 2, 1),
-            });
-
-            builder.HasData(new Customer
-            {
-                FirstName = "Ford",
-                LastName = "Prefect",
-                BirthdayDate = new DateTime(1970, 3, 1),
-            });
-
-            builder.HasData(new Customer
-            {
-                FirstName = "Zaphod",
-                LastName = "Beeblebrox",
-                BirthdayDate = new DateTime(1970, 4, 1),
-            });
-
-            builder.HasData(new Customer
-            {
-                FirstName = "Tricia",
-                LastName = "McMillan",
-                BirthdayDate = new DateTime(1970, 5, 1),
-            });
-
-            builder.HasData(new Customer
-            {
-                FirstName = "Luke",
-                LastName = "Skywalker",
-                BirthdayDate = new DateTime(1970, 6, 1),
-            });
-
-            builder.HasData(new Customer
-            {
-                FirstName = "Han",
-                LastName = "Solo",
-                BirthdayDate = new DateTime(1970, 7, 1),
-            });
-
-            builder.HasData(new Customer
-            {
-                FirstName = "Obiwan",
-                LastName = "Kenobi",
-                BirthdayDate = new DateTime(1970, 8, 1),
-            });
         }
     }
 }
--- PedaloWebApp/Infrastructure/Data/EntityConfigurations/PedaloConfiguration.cs
+++ PedaloWebApp/Infrastructure/Data/EntityConfigurations/PedaloConfiguration.cs
@@ -11,35 +11,6 @@
         {
             builder.HasKey(x => x.PedaloId);
             builder.Property(x => x.Name).IsRequired().HasMaxLength(40);
-
-            Seed(builder);
-        }
-
-        private static void Seed(EntityTypeBuilder<Pedalo> builder)
-        {
-            builder.HasData(new Pedalo
-            {
-                Name = "Heart of Gold",
-                Capacity = 6,
-                Color = PedaloColor.Blue,
-                HourlyRate = 21.00m,
-            });
-
-            builder.HasData(new Pedalo
-            {
-                Name = "Golgafrincham",
-                Capacity = 2,
-                Color = PedaloColor.Pink,
-                HourlyRate = 10.00m,
-            });
-
-            builder.HasData(new Pedalo
-            {
-                Name = "Millennium Falcon",
-                Capacity = 4,
-                Color = PedaloColor.Green,
-                HourlyRate = 17.00m,
-            });
         }
     }
 }
