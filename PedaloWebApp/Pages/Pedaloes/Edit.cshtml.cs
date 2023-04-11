50ea302e0d4e832797514c31b77db80412df7190
Sara <sara.hager@bwz-rappi.ch>
2023-04-11 15:28:06 +0200

    Dropdown Menu for colors

--- PedaloWebApp/Pages/Pedaloes/Edit.cshtml.cs
+++ PedaloWebApp/Pages/Pedaloes/Edit.cshtml.cs
@@ -1,6 +1,7 @@
 namespace PedaloWebApp.Pages.Pedaloes
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
