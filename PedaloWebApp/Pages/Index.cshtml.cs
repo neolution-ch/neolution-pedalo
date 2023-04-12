f927c1582d2d16ab5ad74be941a293eef513dec1
Sara <sara.hager@bwz-rappi.ch>
2023-04-12 15:44:29 +0200

    Dashboard Most Popular Pedaloes Grapic

--- PedaloWebApp/Pages/Index.cshtml.cs
+++ PedaloWebApp/Pages/Index.cshtml.cs
@@ -1,25 +1,48 @@
 ﻿namespace PedaloWebApp.Pages
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
     using Microsoft.Extensions.Logging;
+    using PedaloWebApp.Core.Domain.Entities;
+    using PedaloWebApp.Core.Interfaces.Data;
 
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ILogger<IndexModel> logger;
+        private readonly IDbContextFactory contextFactory;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IDbContextFactory contextFactory)
         {
-            this._logger = logger;
+            this.logger = logger;
+            this.contextFactory = contextFactory;
         }
+
+        public List<PopularPedaloViewModel> PopularPedaloes { get; set; }
 
         public void OnGet()
         {
+            using var context = this.contextFactory.CreateReadOnlyContext();
 
+            // Get the top 3 Pedaloes with the most bookings
+            this.PopularPedaloes = context.Pedaloes
+                .OrderByDescending(p => p.Bookings.Count)
+                .Take(3)
+                .Select(p => new PopularPedaloViewModel
+                {
+                    Name = p.Name,
+                    Color = p.Color.ToString(),
+                    BookingsCount = p.Bookings.Count
+                })
+                .ToList();
         }
     }
+
+    public class PopularPedaloViewModel
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int BookingsCount { get; set; }
+    }
 }
+
