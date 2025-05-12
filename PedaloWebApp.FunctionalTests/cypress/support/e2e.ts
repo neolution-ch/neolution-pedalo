b592746d071d729d76f780d7967b6a1985f03130
Sandro Ciervo <sandro.ciervo@neolution.ch>
2025-05-12 15:17:04 +0200

    Remove unused support class

--- PedaloWebApp.FunctionalTests/cypress/support/e2e.ts
+++ PedaloWebApp.FunctionalTests/cypress/support/e2e.ts
@@ -14,7 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import "./commands/authentication";
 import "./commands/sql";
 import "./commands/form";
 import "./commands/helpers";
